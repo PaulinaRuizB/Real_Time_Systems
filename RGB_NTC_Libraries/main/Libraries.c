fd1641cb6def0c85fd593cfbb20c6f30bb18f318
PaulinaRB <pruizb@unal.co>
2026-05-17 17:10:52 -0500

    Organiza PWM and ADC functions from main.c to Libraries.c

--- RGB_NTC_Libraries/main/Libraries.c
+++ RGB_NTC_Libraries/main/Libraries.c
@@ -9,3 +9,91 @@ static adc_oneshot_unit_handle_t adc_handle;
 static adc_cali_handle_t adc_cali_handle; 
 static bool do_calibration = false;
 
+// Init functions
+
+//ADC INIT 
+static adc_oneshot_unit_handle_t adc_handle;
+static adc_cali_handle_t adc_cali_handle;
+static bool do_calibration = false;
+
+// Colas 
+
+QueueHandle_t temp_queue; 
+QueueHandle_t cmd_queue;
+
+// Calibración ADC 
+static bool example_adc_calibration_init( adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle) { 
+    adc_cali_handle_t handle = NULL; 
+    bool calibrated = false; 
+    
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED 
+    
+    adc_cali_curve_fitting_config_t cali_config = { 
+        .unit_id = unit, 
+        .chan = channel, 
+        .atten = atten, 
+        .bitwidth = ADC_BITWIDTH_DEFAULT, 
+    }; 
+        
+        if (adc_cali_create_scheme_curve_fitting( &cali_config, &handle) == ESP_OK) { 
+            calibrated = true; 
+        } 
+        
+#endif 
+        *out_handle = handle; 
+        return calibrated; 
+    }
+
+    
+// FUNCIÓN CONFIG PWM
+void pwm_init()
+{
+    ledc_timer_config_t timer = {
+        .speed_mode = LEDC_MODE,
+        .timer_num = LEDC_TIMER,
+        .duty_resolution = LEDC_DUTY_RES,
+        .freq_hz = LEDC_FREQUENCY
+    };
+
+    ledc_timer_config(&timer);
+
+    ledc_channel_config_t ch = {
+        .speed_mode = LEDC_MODE,
+        .timer_sel = LEDC_TIMER,
+        .duty = 0,
+        .hpoint = 0
+    };
+
+    ch.channel = LEDC_CHANNEL_R;
+    ch.gpio_num = LED_R;
+    ledc_channel_config(&ch);
+
+    ch.channel = LEDC_CHANNEL_G;
+    ch.gpio_num = LED_G;
+    ledc_channel_config(&ch);
+
+    ch.channel = LEDC_CHANNEL_B;
+    ch.gpio_num = LED_B;
+    ledc_channel_config(&ch);
+}
+
+//CONFIG UART
+
+void uart_init()
+{
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+     uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
+     uart_param_config(UART_NUM_0, &uart_config);
+
+    // Mensaje de bienvenida
+    uart_write_bytes(ECHO_UART_PORT_NUM , "Sistema NTC UART listo. Envia comandos como ROJO_MIN_35 o PWM_50\n", strlen("Sistema NTC UART listo. Envia comandos como ROJO_MIN_35 o PWM_50\n"));
+}
+
